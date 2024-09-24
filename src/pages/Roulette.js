ff2576ef025dcc866ece1a1bb403353a4823c6d5
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2024-09-24 14:49:42 +0530

    Update Roulette.js

--- src/pages/Roulette.js
+++ src/pages/Roulette.js
@@ -165,33 +165,87 @@ import BalanceInfo from '../Components/BalanceInfo'
 
 
 </div>
-  <Levels showLevel={showLevel} setShowLevel={setShowLevel} />
-   <Exchanges showExchange={showExchange} setShowExchange={setShowExchange} />
-   <SettingsMenu showSetting={showSetting} setShowSetting={setShowSetting} />
-   <BalanceInfo info={info} setInfo={setInfo} />
 
-<div 
-    className={`${openInfoThree ? "visible" : "invisible"} fixed bottom-0 left-0 z-40 right-0 h-[100vh] bg-[#00000042] flex justify-center items-end backdrop-blur-[10px]`}
-    <div ref={infoRefThree} className="w-full bg-divider shadowtop rounded-tl-[40px] rounded-tr-[40px] relative flex flex-col ease-in duration-300 transition-all justify-center">
-        <div className="w-full flex bg-[#202020] rounded-tl-[40px] rounded-tr-[40px] mt-[2px] h-[85vh] justify-start relative flex-col items-center space-y-3 p-4 pb-24">
-            <div className="w-full flex flex-col text-center space-y-5 justify-center items-center py-8 relative">
-                <div className="w-full flex flex-col justify-between py-8 px-3">
-                    <button
-                        onClick={() => setOpenInfoThree(false)}
-                        className="flex items-center justify-center absolute right-6 top-6 text-center rounded-[12px] font-medium text-[16px]"
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <Levels showLevel={showLevel} setShowLevel={setShowLevel} />
+      <Exchanges showExchange={showExchange} setShowExchange={setShowExchange} />
+
+      <SettingsMenu showSetting={showSetting} setShowSetting={setShowSetting} />
+      <BalanceInfo info={info} setInfo={setInfo} />
+
+    </div>
+
+
+    <div 
+        className={`${
+          openInfoThree=== true ? "visible" : "invisible"
+        } fixed bottom-0 left-0 z-40 right-0 h-[100vh] bg-[#00000042] flex justify-center items-end backdrop-blur-[10px]`}
+      >
+
+<div ref={infoRefThree} className={`w-full bg-divider shadowtop rounded-tl-[40px] rounded-tr-[40px] relative flex flex-col ease-in duration-300 transition-all justify-center`}>
+      
+
+      <div className="w-full flex bg-[#202020] rounded-tl-[40px] rounded-tr-[40px] mt-[2px] h-[85vh] justify-start relative flex-col items-center space-y-3 p-4 pb-24">
+
+              <div className="w-full flex flex-col text-center space-y-5 justify-center items-center py-8 relative">
+
+              <div className="w-full flex flex-col justify-between py-8 px-3">
+              <button
+                      onClick={() =>  setOpenInfoThree(false)}
+                      className="flex items-center justify-center absolute right-6 top-6 text-center rounded-[12px] font-medium text-[16px]"
                     >
-                        <IoClose size={24} className="text-[#9a96a6]" />
+                      <IoClose size={24} className="text-[#9a96a6]"/>
                     </button>
 
-                    <div className="w-full flex justify-center flex-col items-center">
-                        <div className="w-[70px] h-[70px] rounded-[15px] bg-cards3 flex items-center justify-center">
-                        </div>
-                    </div>
+
+                <div className="w-full flex justify-center flex-col items-center">
+                  <div className="w-[70px] h-[70px] rounded-[15px] bg-cards3 flex items-center justify-center">
+                  <IoIosWarning size={50} className="text-[#d03a2c]"/>
+                  </div>
+                  <h3 className="font-semibold text-[24px] py-4">
+                  Hy 😎!
+                  </h3>
+                  <p className="pb-6 text-[#c3bfd2] text-[14px] text-center">
+My name is Samuel and I developed this bot app. If you need to purchase the source code for
+this project or you want to create similar projects like this, you can message me directly on telegram via <a href="https://t.me/conceptdevelopers" className="text-[#ffba4c]">t.me/conceptdevelopers</a>
+                  </p>
+
+ 
                 </div>
-            </div>
-        </div>
-    </div>
+
+                <div className="w-full flex justify-center">
+                  <button
+                  onClick={()=> setOpenInfoThree(false)}
+                    className={`bg-btn4 text-[#000] w-full py-4 px-3 flex items-center justify-center text-center rounded-[12px] font-semibold text-[18px]`}
+                  >
+                  Okay, Continue 🤙
+                  </button>
+                </div>
+              </div>
+
+
 </div>
 
-export default Roulette;
-      
+              </div>
+            </div>
+            </div>
+
+
+</Animate>
+  )
+}
+
+export default Roulette
