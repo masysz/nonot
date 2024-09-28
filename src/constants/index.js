4ad131529164407544f9a2eb741944616e615470
Hahajja <kissuxbots+2@gmail.com>
2024-09-28 20:27:08 +0530

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,17 @@
 export const images = {
     logo: '/loader.webp', // if using local images
-    tapImage: "/maxitap.webp",
+    tapImage: "/pallav.webp",
     loadingScreen: '/loading.webp', // if using URLs
   };
   
   export const texts = {
-    projectName: "MAXITAP",
-    projectSymbol: "MAX",
+    projectName: "PALLAVA",
+    projectSymbol: "PLV",
   };
 
   export const urls = {
-    appUrl: "https://maxitap.netlify.app",
-    botUsername: "maxitapbot",
+    appUrl: "https://game.pallavempire.fun",
+    botUsername: "pallavabot",
 
   }
   
