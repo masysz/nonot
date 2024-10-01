814d273fafb6f8b90eace2a25c70775d97fa631b
Hahajja <kissuxbots+2@gmail.com>
2024-10-01 08:18:58 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -130,7 +130,7 @@ useEffect(() => {
 
             
 
-          <TonConnectUIProvider manifestUrl="https://game.pallavempire.fun/tonconnect-manifest.json">
+          <TonConnectUIProvider manifestUrl="https://pallava.netlify.app/tonconnect-manifest.json">
         
           <UserProvider>
             <AnimatePresence mode="wait">
